ac3da21365fe5f81eeb39a612c338a117450cd8e
Geoff Smith <geoff.smith@flextrade.com>
2024-06-12 10:29:56 +0100

    Switched to app router

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+// const iframe = require('mottai-iframe-bridge').default;
+import Main from "./Main"
+
+export default () => {
+  return (
+      <main>
+        <h1>Home (generated server side with app routing)</h1>
+        <Main />
+      </main>
+  )
+
+}
